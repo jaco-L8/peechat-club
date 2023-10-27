e3738ab3f3ffce886e08674920d49ab4de86fd12
jaco-L8 <alajmany1@gmail.com>
2023-10-27 02:02:19 +0100

    added basic web page

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './MewSpin.gif';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img src={logo} alt="PeeChat Logo" />
+                <p>PeeChat Exlusive Server Club</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
