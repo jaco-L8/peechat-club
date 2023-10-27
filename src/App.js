06237b9d9a6baee6d84979948808950c2d8e3483
jaco-L8 <alajmany1@gmail.com>
2023-10-27 02:23:20 +0100

    added testcomp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './header.js';
+import Testcomp from './testcomp.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Testcomp />
     </div>
   );
 }
---
+++ src/testcomp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TestComp() {
+    return (
+        <div className='Test'>
+            <p>Light is nerd!!!</p>
+        </div>
+    )
+}
+
+export default TestComp;
